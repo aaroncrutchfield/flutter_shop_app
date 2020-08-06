186432277ecb1d51f8b920cf1e43cf228781931a
acrutchfield <aaroncrutchfield09@gmail.com>
2020-08-06 17:42:14 -0400

    Defining a data model

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/models/product.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
