65c3370fd88b7beb777d7a316077f73dc465b761
acrutchfield <aaroncrutchfield09@gmail.com>
2020-08-06 17:35:01 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/org/flutter_shop_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.flutter_shop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        // Center
+        child: Text('Let\'s build a shop'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+
+  }
+}
