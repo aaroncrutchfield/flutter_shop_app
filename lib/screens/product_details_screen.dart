1bcac0c0113e3aa2441dd73e69402585bd893782
acrutchfield <aaroncrutchfield09@gmail.com>
2020-08-07 11:48:21 -0400

    Fix error from calling read in build method

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shop_app/models/product.dart';
 import 'package:flutter_shop_app/providers/products_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -11,7 +10,7 @@ class ProductDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 	  final productId = ModalRoute.of(context).settings.arguments as String;
-	  final product = context.read<ProductsProvider>().findById(productId);
+	  final product = context.select((ProductsProvider provider) => provider.findById(productId));
 
     return Scaffold(
 	    appBar: AppBar(title: Text(product.title),),
