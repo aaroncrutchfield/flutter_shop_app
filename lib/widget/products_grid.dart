6f4bbb9aa531db223eea071df0553d4561b9a68d
acrutchfield <aaroncrutchfield09@gmail.com>
2020-08-07 11:57:42 -0400

    Alternative syntax for list/grid efficiency

--- lib/widget/products_grid.dart
+++ lib/widget/products_grid.dart
@@ -15,8 +15,8 @@ class ProductsGrid extends StatelessWidget {
 		return GridView.builder(
 			padding: const EdgeInsets.all(10),
 			itemCount: products.length,
-			itemBuilder: (ctx, index) => ChangeNotifierProvider(
-			  create: (ctx) => products[index],
+			itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+			  value: products[index],
 				child: ProductItem(),
 			),
 			gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
